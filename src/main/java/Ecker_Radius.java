d771df711c32642744ac84093597e8aaa9812ef0
QuirinEcker <quirin.ecker@bajupa.com>
2017-10-27 23:05:23 +0200

    initial code

---
+++ src/main/java/Ecker_Radius.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ecker_Radius {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double umfang;
+        double flaeche;
+
+        System.out.print("Radius");
+        double radius = scanner.nextDouble();
+
+        umfang =
+
+
+    }
+}
