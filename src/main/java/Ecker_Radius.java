ce3f13a29b3b2f724e3452d71f52429ee3710a6a
QuirinEcker <quirin.ecker@bajupa.com>
2017-10-27 23:24:39 +0200

    complete code

--- src/main/java/Ecker_Radius.java
+++ src/main/java/Ecker_Radius.java
@@ -1,4 +1,24 @@
+/*----------------------------------------------------------
+ *                 HTL-Leonding / Klasse: 1AHITM
+ * ----------------------------------------------------------
+ * Übungsnr.:   13
+ * Übungstitel: Radius
+ * Autoren:     Quirin Ecker
+ * Version:     1.0
+ * Datum:       27.10.2017
+ * ----------------------------------------------------------
+ * Kurzbeschreibung:
+ * Berechnung des Umfangs und der Fläche
+ *
+ * ----------------------------------------------------------
+ * Beschreibung der grundlegenden Lösungsidee:
+ * Berechnung des Umfangs und der Fläche mit Math.PI.
+ *
+ * ----------------------------------------------------------
+*/
+
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Ecker_Radius {
 
@@ -7,11 +27,24 @@ public class Ecker_Radius {
 
         double umfang;
         double flaeche;
+        String ausgabeumfang;
+        String ausgabeflaeche;
 
-        System.out.print("Radius");
+        System.out.print("Radius: ");
         double radius = scanner.nextDouble();
 
-        umfang =
+        umfang = 2 * radius * Math.PI;
+        ausgabeumfang = "Umfang des Kreises : ";
+
+        flaeche = radius * radius * Math.PI;
+        ausgabeflaeche = "Flaeche des Kreises : ";
+
+
+        System.out.printf("%s %.2f mm \n" , ausgabeumfang , umfang);
+        System.out.printf("%s %.2f mm \n" , ausgabeflaeche , flaeche);
+
+
+
 
 
     }
